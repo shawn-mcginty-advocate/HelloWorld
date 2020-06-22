e013d180c10b0db6673c287849ec7063bb959bf9
shawn-mcginty-advocate <55758542+shawn-mcginty-advocate@users.noreply.github.com>
2020-06-22 02:01:20 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
